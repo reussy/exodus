81897fbd13ca2e78437ce33c76ecc0be67b8f5a0
reussy <ricardoaraque@outlook.com>
2022-07-24 22:49:58 -0600

    🎒 | Added Api version

--- src/main/java/com/reussy/development/bukkit/plugin/ExodusJavaPlugin.java
+++ src/main/java/com/reussy/development/bukkit/plugin/ExodusJavaPlugin.java
@@ -4,6 +4,7 @@ import com.reussy.development.api.ExodusAPI;
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.Website;
@@ -12,6 +13,7 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author;
 @Plugin(name = "exodus", version = "1.0.0-SNAPSHOT")
 @Description("API for easier creation of plugins.")
 @Author("Ricardo (reussy)")
+@ApiVersion(ApiVersion.Target.v1_18)
 @Website("reussy.me")
 
 public class ExodusJavaPlugin extends JavaPlugin {
